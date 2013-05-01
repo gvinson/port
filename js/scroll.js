ad17865b52a003c3a6f58ec4068c01c10691e207
gvinson <vinsongrant@gmail.com>
2013-04-30 20:41:37 -0500

    updated scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -10,53 +10,61 @@ $(document).ready(function() {
 		//show/hide section titles depending on position on site
 		$(window).scroll(function(){
 			//Show section titles when viewing that section
-			for(var i=0; i < sectionTitles.length; i++) {		
+			$(".sectionTitle").each(function(index, prevDom) {		
 				var yOffset = 600;	//yOffset for works section
-				var h = $('#'+sectionTitles[i]).parent().parent().height();	//height of section scrolling past
-				var y = $(window).scrollTop() - $("#" + sectionTitles[i]).parent().parent().offset().top+yOffset+(document.documentElement.clientHeight*.05); //height to top
+				var h = $(this).parent().parent().height();	//height of section scrolling past
+				var y = $(window).scrollTop() - $(this).parent().parent().offset().top+yOffset+(document.documentElement.clientHeight*.05); //height to top
 				
-				if(i!=0)
-					yOffset = 500;	//yOffset for posts section
-
+				//make title appear on left side of content ONLY if
+				//the browser window is wider than 1240
 				if(document.documentElement.clientWidth > 1240) {
 					if( y > (h*.25) && y < (h) ){
-						// if we are show sectionTitle
-						var marginTop = ($('#'+sectionTitles[i]).parent().parent().offset().top-200)*(-1);
+						//distance between the top of the section title and the top of the document
+						var marginTop = ($(this).parent().parent().offset().top-200)*(-1);
 							
-						$('#'+sectionTitles[i]).fadeIn(300).css({
+						$(this).fadeIn(300).css({
 							"position":"fixed",
 							"margin-top": marginTop
 						});
 					} else {
-						$('#'+sectionTitles[i]).fadeOut(300);
+						$(this).fadeOut(300);
 					}
 				} 
 				else {
 					if( y >= (h*.2) )
 					{
-						var marginLeft = $("#"+sectionTitles[i]).parent().parent().find(".wrapper");
+						var marginLeft = $(this).parent().parent().find(".wrapper");
 						marginLeft = marginLeft.offset().left;
+						var titleOffset = $(this).offset().top;
+						var marginBottom = $(this).parent().parent().css("margin-bottom").substring(0,2);
+						var endSection = $(this).parent().parent().height();
 						
-						$('#'+sectionTitles[i]).fadeIn(300).css({
+						$(this).fadeIn(300).css({
 							"margin-left":marginLeft
 						});
+						
+						//if the section title meets the top of the window,
+						//make it a fixed white bar at the top
+						
+						if(titleOffset < $(window).scrollTop()) {
+							$(this).css({
+								"height":"1em",
+								"background-color":"#ffffff",
+								"position":"fixed",
+								"z-index":"50",
+								"top":"1em",
+								"padding-bottom":".1em"
+							});
+							
+							
+						}
 					}
 					else
-						$('#'+sectionTitles[i]).fadeOut(300);
-						
-					if($('#'+sectionTitles[i]).offset().top < $(window).scrollTop()) {
-						$('#'+sectionTitles[i]).css({
-							"height":"1em",
-							"background-color":"#ffffff",
-							"position":"fixed",
-							"z-index":"50",
-							"top":"1em",
-							"padding-bottom":".1em"
-						});
-					}
+						$(this).fadeOut(300);
+					
 				}
 				
-			}
+			});
 			
 			//display scrollToTop link when at work section
 			if(document.documentElement.clientWidth > 1240) {
